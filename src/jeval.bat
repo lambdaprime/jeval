9cf386b706dc05457bd5131415a2c026db32b85b
lambdaprime <id.blackmesa@gmail.com>
2019-11-12 19:48:31 -0800

    Fixing Windows launcher

--- src/jeval.bat
+++ src/jeval.bat
@@ -1,1 +1,1 @@
-java  -Xmn64m -Xms128m -Xmx512m -Xss1024k -XX:+UseParallelGC -Xnoclassgc -Xshare:off -noverify -jar "jeval.jar" %*
+java  -Xmn64m -Xms128m -Xmx512m -Xss1024k -XX:+UseParallelGC -Xnoclassgc -Xshare:off -noverify -cp "%~dp0\jeval.jar";%CLASSPATH% id.jeval.Main %*
