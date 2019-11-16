e255ed8e6af13f25078020d9527eb7c1601328e8
lambdaprime <id.blackmesa@gmail.com>
2019-11-15 21:33:04 -0800

    Version 13: Switch to new license

--- src/java/id/jeval/JshExecutor.java
+++ src/java/id/jeval/JshExecutor.java
@@ -1,24 +1,18 @@
-/* 
- * jeval - java code evaluator
- *
- * This source file is a part of jeval command line program.
- * Description for this  project/command/program can be found in README.org
- *
- * jeval is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * jeval is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with jeval. If not, see <http://www.gnu.org/licenses/>.
- *
+/*
+ * Copyright 2019 lambdaprime
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package id.jeval;
 
 import static jdk.jshell.SourceCodeAnalysis.Completeness.COMPLETE;
